676ae694fd84eddad0f8ca68ef68fc3a2d8fbebc
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-21 19:56:22 -0300

    elimitado arquivos padrão de projeto React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <h1>Hello React</h1>
     </div>
   );
 }
